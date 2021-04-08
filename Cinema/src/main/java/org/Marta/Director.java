85682262638adbd18de14425aef6d9485f9a4125
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-04-08 17:16:42 +0200

    classes Movie and Director added

---
+++ Cinema/src/main/java/org/Marta/Director.java
@@ -0,0 +1,11 @@
+package org.Marta;
+
+public class Director {
+    private String name;
+    private String surname;
+
+    public Director(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+}
