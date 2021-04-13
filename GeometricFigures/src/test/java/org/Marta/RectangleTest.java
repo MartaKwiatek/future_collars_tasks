e606a3a692fd030fcc26d2a097c4374aa41067f3
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-04-13 12:57:04 +0200

    Shape class and inheriting classes added

---
+++ GeometricFigures/src/test/java/org/Marta/RectangleTest.java
@@ -0,0 +1,34 @@
+package org.Marta;
+
+import org.apache.commons.math3.util.Precision;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+    private Rectangle rectangle;
+
+    @BeforeEach
+    public void setRectangle() {
+        rectangle = new Rectangle(3, 5);
+    }
+
+    @Test
+    void getAreaTest() {
+        double result = rectangle.getArea();
+
+        Assertions.assertEquals(15.00, Precision.round(result, 2));
+    }
+
+    @Test
+    void getPerimeterTest() {
+        double result = rectangle.getPerimeter();
+
+        Assertions.assertEquals(16.00, Precision.round(result, 2));
+    }
+}
---
+++ GeometricFigures/src/test/java/org/Marta/TriangleTest.java
@@ -0,0 +1,31 @@
+package org.Marta;
+
+import org.apache.commons.math3.util.Precision;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangleTest {
+    private Triangle triangle;
+
+    @BeforeEach
+    public void setTriangle() {
+        triangle = new Triangle(4, 5, 6);
+    }
+
+    @Test
+    void getAreaTest() {
+        double result = triangle.getArea();
+
+        Assertions.assertEquals(9.92, Precision.round(result,2));
+    }
+
+    @Test
+    void getPerimeterTest() {
+        double result = triangle.getPerimeter();
+
+        Assertions.assertEquals(15.00, Precision.round(result,2));
+    }
+}
