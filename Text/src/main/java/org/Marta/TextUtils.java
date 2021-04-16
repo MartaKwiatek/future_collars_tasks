c0d668240ef25a66c324f9a782b246953e853407
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-04-16 11:51:29 +0200

    split text preparation into methods

--- Text/src/main/java/org/Marta/TextUtils.java
+++ Text/src/main/java/org/Marta/TextUtils.java
@@ -1,16 +1,30 @@
 package org.Marta;
 
+import java.util.Locale;
+
 public class TextUtils {
     public static boolean isNotEmpty(String text) {
         return text.length() != 0;
     }
 
+    private static String removeWhiteSpaces(String text) {
+        return text.replaceAll("\\s", "");
+    }
+
+    private static String removePunctuationMarks(String text) {
+        return text.replaceAll("\\p{Punct}", "");
+    }
+
     public static boolean isPalindrome(String text) {
-        String preparedText = text.replaceAll("\\s", "").replaceAll("\\p{Punct}", "").toLowerCase();
-        StringBuilder stringBuilder = new StringBuilder(preparedText);
-        stringBuilder.reverse();
-        String reversedText = stringBuilder.toString();
-        return preparedText.equals(reversedText);
+        if (isNotEmpty(text)) {
+            String preparedText = removePunctuationMarks(removeWhiteSpaces(text)).toLowerCase();
+            StringBuilder stringBuilder = new StringBuilder(preparedText);
+            stringBuilder.reverse();
+            String reversedText = stringBuilder.toString();
+            return preparedText.equals(reversedText);
+        }
+        else
+            return false;
     }
 
     public static int textLength(String text) {
--- Text/src/test/java/org/Marta/TextUtilsTest.java
+++ Text/src/test/java/org/Marta/TextUtilsTest.java
@@ -8,45 +8,42 @@ class TextUtilsTest {
     @Test
     void shouldReturnTextIsEmpty() {
         String text = "";
-
         boolean isNotEmpty = TextUtils.isNotEmpty(text);
-
         Assertions.assertFalse(isNotEmpty);
     }
 
     @Test
     void shouldReturnTextIsNotEmpty() {
         String text = "Future Collars";
-
         boolean isNotEmpty = TextUtils.isNotEmpty(text);
-
         Assertions.assertTrue(isNotEmpty);
     }
 
     @Test
     void shouldReturnTextIsAPalindrome() {
         String text = "Dammit, I'm mad.";
-
         boolean isPalindrome = TextUtils.isPalindrome(text);
-
         Assertions.assertTrue(isPalindrome);
     }
 
     @Test
     void shouldReturnTextIsNotAPalindrome() {
         String text = "Konstantynopolitanczykowianeczka";
-
         boolean isPalindrome = TextUtils.isPalindrome(text);
+        Assertions.assertFalse(isPalindrome);
+    }
 
+    @Test
+    void shouldReturnEmptyTextIsNotAPalindrome() {
+        String text = "";
+        boolean isPalindrome = TextUtils.isPalindrome(text);
         Assertions.assertFalse(isPalindrome);
     }
 
     @Test
     void shouldReturnTextLengthIs13() {
         String text = "Marta Kwiatek";
-
         int length = TextUtils.textLength(text);
-
         Assertions.assertEquals(13, length);
     }
 }
