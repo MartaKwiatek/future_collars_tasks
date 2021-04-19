140466dff3f727a4f9b170c40fda754e0950d08d
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-04-19 10:45:09 +0200

    removed unnecessary braces

--- FizzBuzz/FizzBuzz/src/main/java/org/Marta/FizzBuzzPrinter.java
+++ FizzBuzz/FizzBuzz/src/main/java/org/Marta/FizzBuzzPrinter.java
@@ -3,15 +3,12 @@ package org.Marta;
 public class FizzBuzzPrinter {
     public static void printFizzBuzz() {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 3 == 0 && i % 5 == 0)
                 System.out.println("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
+            else if (i % 3 == 0)
                 System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
+            else if (i % 5 == 0)
                 System.out.println("Buzz");
-            }
             else
                 System.out.println(i);
         }
