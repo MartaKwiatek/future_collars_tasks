454a18366aaeb12e8784a7103083b062c602b608
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-04-23 11:56:46 +0200

    paths changed

--- DisplayEverySecondLineFromTxtFile/src/main/java/org/Marta/App.java
+++ DisplayEverySecondLineFromTxtFile/src/main/java/org/Marta/App.java
@@ -1,6 +1,5 @@
 package org.Marta;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,19 +7,20 @@ import java.nio.file.Paths;
 public class App {
     public static void main(String[] args) {
         try {
-            Path file = Paths.get("C:\\Users\\marta\\git\\future_collars_tasks\\CreateTxtFile\\data.txt");
+            Path file = Paths.get("..\\CreateTxtFile\\data.txt");
             long countLines = Files.lines(file).count();
             System.out.println("Total lines: " + countLines);
 
             int n = 0;
             while (n < countLines) {
-                String line = Files.readAllLines(Paths.get("C:\\Users\\marta\\git\\future_collars_tasks\\CreateTxtFile\\data.txt")).get(n);
+                String line = Files.readAllLines(Paths.get("..\\CreateTxtFile\\data.txt")).get(n);
                 System.out.println(line);
                 n = n + 2;
             }
 
         } catch (Exception e) {
             e.getStackTrace();
+
         }
     }
 }
