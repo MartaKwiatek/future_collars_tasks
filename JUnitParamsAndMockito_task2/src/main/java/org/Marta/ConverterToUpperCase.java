c0f6f1ad299a533a595cbcb64798e20f70cabe72
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-04-27 10:33:12 +0200

    fixed tests

--- JUnitParamsAndMockito_task2/src/main/java/org/Marta/ConverterToUpperCase.java
+++ JUnitParamsAndMockito_task2/src/main/java/org/Marta/ConverterToUpperCase.java
@@ -2,7 +2,7 @@ package org.Marta;
 
 public class ConverterToUpperCase {
     public static String convertToUpperCase(String text) {
-        if (text == null || text.length() == 0) {
+        if (text == null) {
             return "";
         }
         return text.toUpperCase();
--- JUnitParamsAndMockito_task2/src/test/java/org/Marta/ConverterToUpperCaseTest.java
+++ JUnitParamsAndMockito_task2/src/test/java/org/Marta/ConverterToUpperCaseTest.java
@@ -10,18 +10,20 @@ class ConverterToUpperCaseTest {
     @ParameterizedTest
     @ValueSource(strings = {"maRta", "Future Collars", "JavA"})
     void shouldVerifyTextIsConvertedToUpperCase(String input) {
+        //given, when
         String expected = input.toUpperCase();
         String actual = ConverterToUpperCase.convertToUpperCase(input);
-
+        //then
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     void shouldReturnEmptyString(String input) {
+        //given, when
         String expected = "";
         String actual = ConverterToUpperCase.convertToUpperCase(input);
-
+        //then
         Assertions.assertEquals(expected, actual);
     }
 }
