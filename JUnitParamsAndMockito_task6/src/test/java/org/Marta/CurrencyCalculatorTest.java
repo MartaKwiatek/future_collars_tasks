28a4e2bc6fbfd75830c8fd547301f0c79b1351b5
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-05-06 20:00:35 +0200

    Fixed tests with Method Source

--- JUnitParamsAndMockito_task6/src/test/java/org/Marta/CurrencyCalculatorTest.java
+++ JUnitParamsAndMockito_task6/src/test/java/org/Marta/CurrencyCalculatorTest.java
@@ -2,54 +2,57 @@ package org.Marta;
 
 import org.Marta.externalServices.ExchangeRate;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 class CurrencyCalculatorTest {
 
-    @Test
-    void shouldConvertPlnInBuyingRate() {
-        //given
+    @ParameterizedTest
+    @MethodSource("provideCalculatedBuyingRate")
+    void shouldConvertPlnInBuyingRate(BigDecimal expectedAmount, BigDecimal calculatedAmount) {
+        Assertions.assertEquals(expectedAmount, calculatedAmount);
+    }
+
+    private static Stream<Arguments> provideCalculatedBuyingRate() {
         BigDecimal amountOfPLN = new BigDecimal("20.0");
         ExchangeRate exchangeRateMock = Mockito.mock(ExchangeRate.class);
+        CurrencyCalculator currencyCalculator = new CurrencyCalculator(exchangeRateMock);
 
         Mockito.when(exchangeRateMock.getBuyingRate(Currency.EUR)).thenReturn(new BigDecimal("4.57"));
         Mockito.when(exchangeRateMock.getBuyingRate(Currency.USD)).thenReturn(new BigDecimal("3.80"));
         Mockito.when(exchangeRateMock.getBuyingRate(Currency.GBP)).thenReturn(new BigDecimal("5.28"));
-        CurrencyCalculator currencyCalculator = new CurrencyCalculator(exchangeRateMock);
 
-        //when
-        BigDecimal buyingRateEuro = currencyCalculator.calculateBuyingRate(amountOfPLN, Currency.EUR);
-        BigDecimal buyingRateDollar = currencyCalculator.calculateBuyingRate(amountOfPLN, Currency.USD);
-        BigDecimal buyingRatePound = currencyCalculator.calculateBuyingRate(amountOfPLN, Currency.GBP);
-
-        //then
-        Assertions.assertEquals(new BigDecimal("91.400"), buyingRateEuro);
-        Assertions.assertEquals(new BigDecimal("76.000"), buyingRateDollar);
-        Assertions.assertEquals(new BigDecimal("105.600"), buyingRatePound);
+        return Stream.of(
+                Arguments.of(new BigDecimal("91.400"), currencyCalculator.calculateBuyingRate(amountOfPLN, Currency.EUR)),
+                Arguments.of(new BigDecimal("76.000"), currencyCalculator.calculateBuyingRate(amountOfPLN, Currency.USD)),
+                Arguments.of(new BigDecimal("105.600"), currencyCalculator.calculateBuyingRate(amountOfPLN, Currency.GBP))
+        );
     }
 
-    @Test
-    void shouldConvertPlnInSellingRate() {
-        //given
+    @ParameterizedTest
+    @MethodSource("provideCalculatedSellingRate")
+    void shouldConvertPlnInSellingRate(BigDecimal expectedAmount, BigDecimal calculatedAmount) {
+        Assertions.assertEquals(expectedAmount, calculatedAmount);
+    }
+
+    private static Stream<Arguments> provideCalculatedSellingRate() {
         BigDecimal amountOfPLN = new BigDecimal("20.0");
         ExchangeRate exchangeRateMock = Mockito.mock(ExchangeRate.class);
+        CurrencyCalculator currencyCalculator = new CurrencyCalculator(exchangeRateMock);
 
         Mockito.when(exchangeRateMock.getSellingRate(Currency.EUR)).thenReturn(new BigDecimal("4.59"));
         Mockito.when(exchangeRateMock.getSellingRate(Currency.USD)).thenReturn(new BigDecimal("3.82"));
         Mockito.when(exchangeRateMock.getSellingRate(Currency.GBP)).thenReturn(new BigDecimal("5.32"));
-        CurrencyCalculator currencyCalculator = new CurrencyCalculator(exchangeRateMock);
 
-        //when
-        BigDecimal sellingRateEuro = currencyCalculator.calculateSellingRate(amountOfPLN, Currency.EUR);
-        BigDecimal sellingRateDollar = currencyCalculator.calculateSellingRate(amountOfPLN, Currency.USD);
-        BigDecimal sellingRatePound = currencyCalculator.calculateSellingRate(amountOfPLN, Currency.GBP);
-
-        //then
-        Assertions.assertEquals(new BigDecimal("91.800"), sellingRateEuro);
-        Assertions.assertEquals(new BigDecimal("76.400"), sellingRateDollar);
-        Assertions.assertEquals(new BigDecimal("106.400"), sellingRatePound);
+        return Stream.of(
+                Arguments.of(new BigDecimal("91.800"), currencyCalculator.calculateSellingRate(amountOfPLN, Currency.EUR)),
+                Arguments.of(new BigDecimal("76.400"), currencyCalculator.calculateSellingRate(amountOfPLN, Currency.USD)),
+                Arguments.of(new BigDecimal("106.400"), currencyCalculator.calculateSellingRate(amountOfPLN, Currency.GBP))
+        );
     }
 }
