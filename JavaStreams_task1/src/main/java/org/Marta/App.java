bdbc5baadb68bfedbc09418b52d42397cafa4665
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-05-17 16:36:48 +0200

    add creation of the collection

--- JavaStreams_task1/src/main/java/org/Marta/App.java
+++ JavaStreams_task1/src/main/java/org/Marta/App.java
@@ -1,12 +1,16 @@
 package org.Marta;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) {
         IntStream ints = IntStream.range(-20, 21);
-        ints
+        List<Integer> filteredNums =  ints
                 .filter(num -> num % 3 == 0)
-                .forEach(System.out::println);
+                .boxed()
+                .collect(Collectors.toList());
+        filteredNums.forEach(System.out::println);
     }
 }
