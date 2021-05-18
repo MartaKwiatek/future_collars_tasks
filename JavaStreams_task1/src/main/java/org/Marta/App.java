1402a8a7510568454258e98bb5eb429f511f6dc4
Marta Kwiatek <marta.kwiatek13@gmail.com>
2021-05-18 11:31:43 +0200

    add method filterNums

--- JavaStreams_task1/src/main/java/org/Marta/App.java
+++ JavaStreams_task1/src/main/java/org/Marta/App.java
@@ -5,12 +5,14 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class App {
-    public static void main(String[] args) {
-        IntStream ints = IntStream.range(-20, 21);
-        List<Integer> filteredNums =  ints
+    public static List<Integer> filterNums(int from, int to) {
+        return IntStream.range(from, to)
                 .filter(num -> num % 3 == 0)
                 .boxed()
                 .collect(Collectors.toList());
-        filteredNums.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        filterNums(-20, 21).forEach(System.out::println);
     }
 }
